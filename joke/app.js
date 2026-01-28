69f879dab9d8259a072af338f4bcf712434a5d03
kunalmaurya6 <mauryaankit86810@gmail.com>
2026-01-28 17:20:53 +0530

    added button

--- joke/app.js
+++ joke/app.js
@@ -1,6 +1,12 @@
 fetch("https://icanhazdadjoke.com/slack")
-.then(data=>data.json())
-.then(joke=>{
-    const jokeElement=document.getElementById("jokeElement");
-    jokeElement.innerText=joke.attachments[0].text;
-})
+    .then(data => data.json())
+    .then(joke => {
+        const jokeElement = document.getElementById("jokeElement");
+        const bt = document.getElementsByTagName("h1");
+        const btn = document.createElement("button");
+        btn.className = `btn`;
+        btn.innerHTML = `<i class="fa-solid fa-arrows-rotate"></i>`
+        btn.addEventListener("click", () => location.reload());
+        bt[0].appendChild(btn);
+        jokeElement.innerHTML = joke.attachments[0].text;
+    }).catch(e => console.log(e.message));
